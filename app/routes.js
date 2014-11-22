48b205469cb81ab8b868dad9119337b445505163
basin0809 <wangxipeng0809@gmail.com>
2014-11-21 19:35:55 -0500

    update

--- app/routes.js
+++ app/routes.js
@@ -13,7 +13,7 @@ var openShiftConnection = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
     process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
     process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
     process.env.OPENSHIFT_APP_NAME;
-mongoose.connect(localConnection);
+mongoose.connect(openShiftConnection);
 module.exports = function (app, passport, server) {
     var conn = mongoose.connection;
     var paintings = mongoose.model('Paintings',
